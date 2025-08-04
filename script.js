c39d70b035bf2e04b64ceb3b56ed6e14532c557f
Supriyo Dana <supriyodana@gmail.com>
2025-08-04 22:08:18 +0530

    v2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function goToApp(url) {
   window.location.href = url;
 }
+
+
+function showUserName() {
+      const name = localStorage.getItem("userName");
+      if (!name) {
+        window.location.href = "index.html";
+      } else {
+        document.querySelector(".userNameDisplay").innerText = `Welcome, ${name}`;
+      }
+    }
